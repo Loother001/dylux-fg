c0667a3cf2c020b1dca71084add35c652b84b9d9
Loother001 <122788306+Loother001@users.noreply.github.com>
2023-03-16 18:12:00 +0500

    Update config.js

--- config.js
+++ config.js
@@ -3,13 +3,13 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['59172945992', 'FG98', true],
-  ['5492622271736'], 
-  ['5492622271736'] 
+  ['923247653527', 'FG98', true],
+  ['923466056497'], 
+  ['923247653527'] 
 ] //Numeros de owner 
 
-global.mods = ['573125484672'] 
-global.prems = ['50489079501', '5219631769130', '573125484672']
+global.mods = ['923247653527'] 
+global.prems = ['923466056497', '923247653527', '923154944140']
 global.APIs = { // API Prefix
   // name: 'https://website'
   xteam: 'https://api.xteam.xyz', 
@@ -27,10 +27,10 @@ global.APIKeys = { // APIKey Here
 // Sticker WM
 global.packname = 'DyLux┃ᴮᴼᵀ' 
 global.author = '@fg98' 
-global.fgig = '▢ Sígueme en Instagram\nhttps://www.instagram.com/fg98_ff\n' 
-global.dygp = 'https://chat.whatsapp.com/IO9jmpI72ejHiN4unRZleU'
-global.fgsc = 'https://github.com/FG98F/dylux-fg' 
-global.fgyt = 'https://youtube.com/fg98f'
+global.fgig = '▢ Sígueme en Instagram\nhttps://chat.whatsapp.com/Ds8Zc29apIvHSFNByv8SEO\n' 
+global.dygp = 'https://chat.whatsapp.com/Ds8Zc29apIvHSFNByv8SEO'
+global.fgsc = 'https://chat.whatsapp.com/Ds8Zc29apIvHSFNByv8SEO' 
+global.fgyt = 'https://chat.whatsapp.com/Ds8Zc29apIvHSFNByv8SEO'
 global.fgpyp = 'https://paypal.me/fg98f'
 global.fglog = 'https://i.imgur.com/Owmb93c.png' 
 
